cfaaafef38eca656931042a2dfdbdb1deb8a854e
Arina29 <arina.chirosca@gmail.com>
2018-11-06 22:03:54 +0200

    Update ex 2 and 5

--- lab5/ex2.sql
+++ lab5/ex2.sql
@@ -4,7 +4,7 @@ Set @evaluarea = 'Testul 1';
 Set @Nota1 = 6;
 Set @Nota2 = 8;
 
-select top 10 Nume_Student, Prenume_Student, Nota
+select top 10 Nume_Student, Prenume_Student
 from studenti
 join studenti_reusita on studenti.Id_Student = studenti_reusita.Id_Student
 join discipline on studenti_reusita.Id_Disciplina = discipline.Id_Disciplina
